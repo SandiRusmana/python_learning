0b9b5ec7d9f5eb48ae6aa3fe892d86ca484c0e21
SandiRusmana <sandirusmana535@gmail.com>
2024-10-14 09:12:23 +0700

    koding limas

---
+++ limas.py
@@ -0,0 +1,9 @@
+print("="*25)
+print("luas permukaan limas")
+print("="*25)
+
+la = int(input("masukan nilai luas alas\t\t:"))
+ls = int(input("masulan nilai luas sisi tegak\t:"))
+
+luas_permukaan = la + ls
+print("luas permukaan\t\t\t:",luas_permukaan)
